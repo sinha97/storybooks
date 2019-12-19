208b17f7708619c29c453d935b5d701a38c9da72
sinha97 <vk9633698@gmail.com>
2019-12-19 15:02:23 +0530

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://dugu:zxcvbnm@1234@cluster0-bqjk9.mongodb.net/test?retryWrites=true&w=majority',
+  googleClientID:
+    '45189326732-q1c0ao428h3a2e003lc9p05feh014abg.apps.googleusercontent.com',
+  googleClientSecret: 'aH44U5U9M_k0gg7K-aLcwapX'
+};
