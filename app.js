a325169d25663bd47de93e575a5a0ff2c6290d22
sinha97 <vk9633698@gmail.com>
2019-12-30 12:57:25 +0530

    Started UI

--- app.js
+++ app.js
@@ -1,20 +1,63 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
+
+//Load user model
+require('./models/User');
 
 // Passport Config
 require('./config/passport')(passport);
 
 //load routes
+const index = require('./routes/index');
 const auth = require('./routes/auth');
+
+// Load keys
+const keys = require('./config/keys');
+
+// Map global promise
+mongoose.promise = global.Promise;
+// Mongoose connect
+mongoose
+  .connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('It Works');
+// Handlebars Middleware
+app.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'main'
+  })
+);
+app.set('view engine', 'handlebars');
+
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
 });
 
 // Use Routes
+app.use('/', index);
 app.use('/auth', auth);
 
 const port = process.env.PORT || 5005;
--- routes/auth.js
+++ routes/auth.js
@@ -11,8 +11,21 @@ router.get(
   '/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
-    res.redirect('dashboard');
+    res.redirect('/dashboard');
   }
 );
+
+router.get('/verify', (req, res) => {
+  if (req.user) {
+    console.log(req.user);
+  } else {
+    console.log('Not Auth');
+  }
+});
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router;
