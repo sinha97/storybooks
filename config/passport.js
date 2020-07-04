953240c3187d3a5e4d0c1120cc2e5d54a9ee9ec6
sinha97 <vk9633698@gmail.com>
2020-07-04 21:19:01 +0530

    storybook webapp with google auth

--- config/passport.js
+++ config/passport.js
@@ -4,42 +4,46 @@ const keys = require('./keys');
 // Load user model
 const User = mongoose.model('users');
 
-module.exports = function(passport){
+module.exports = function(passport) {
   passport.use(
-    new GoogleStrategy({
-      clientID: keys.googleClientID,
-      clientSecret:keys.googleClientSecret,
-      callbackURL:'/auth/google/callback',
-      proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
-      // console.log(accessToken);
-      // console.log(profile);
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback',
+        proxy: true
+      },
+      (accessToken, refreshToken, profile, done) => {
+        // console.log(accessToken);
+        // console.log(profile);
 
-      const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
-      
-      const newUser = {
-        googleID: profile.id,
-        firstName: profile.name.givenName,
-        lastName: profile.name.familyName,
-        email: profile.emails[0].value,
-        image: image
+        const image = profile.photos[0].value.substring(
+          0,
+          profile.photos[0].value.indexOf('?')
+        );
+
+        const newUser = {
+          googleID: profile.id,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          email: profile.emails[0].value,
+          image: image
+        };
+
+        // Check for existing user
+        User.findOne({
+          googleID: profile.id
+        }).then(user => {
+          if (user) {
+            // Return user
+            done(null, user);
+          } else {
+            // Create user
+            new User(newUser).save().then(user => done(null, user));
+          }
+        });
       }
-
-      // Check for existing user
-      User.findOne({
-        googleID: profile.id
-      }).then(user => {
-        if(user){
-          // Return user
-          done(null, user);
-        } else {
-          // Create user
-          new User(newUser)
-            .save()
-            .then(user => done(null, user));
-        }
-      })
-    })
+    )
   );
 
   passport.serializeUser((user, done) => {
@@ -49,4 +53,4 @@ module.exports = function(passport){
   passport.deserializeUser((id, done) => {
     User.findById(id).then(user => done(null, user));
   });
-}
+};
--- models/Story.js
+++ models/Story.js
@@ -3,41 +3,43 @@ const Schema = mongoose.Schema;
 
 // Create Shema
 const StorySchema = new Schema({
-  title:{
-    type:String,
+  title: {
+    type: String,
     required: true
   },
-  body:{
+  body: {
     type: String,
     required: true
   },
   status: {
     type: String,
-    default:'public'
+    default: 'public'
   },
   allowComments: {
     type: Boolean,
-    default:true
+    default: true
   },
-  comments: [{
-    commentBody: {
-      type: String,
-      required: true
-    },
-    commentDate:{
-      type: Date,
-      default: Date.now
-    },
-    commentUser:{
-      type: Schema.Types.ObjectId,
-      ref:'users'
+  comments: [
+    {
+      commentBody: {
+        type: String,
+        required: true
+      },
+      commentDate: {
+        type: Date,
+        default: Date.now
+      },
+      commentUser: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
     }
-  }],
-  user:{
+  ],
+  user: {
     type: Schema.Types.ObjectId,
-    ref:'users'
+    ref: 'users'
   },
-  date:{
+  date: {
     type: Date,
     default: Date.now
   }
